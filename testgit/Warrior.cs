ebca398d991254eb456d9a53bafa690b5c857fd4
nickraz <obik.paren@gmail.com>
2015-11-03 14:22:12 +0300

    Добавил еще показателей + некоторые комментарии

--- testgit/Warrior.cs
+++ testgit/Warrior.cs
@@ -7,12 +7,33 @@ namespace testgit
 {
     class Warrior
     {
+        /// <summary>
+        /// Показатель максимального запаса здоровья
+        /// </summary>
         public int MaxHealth { get; set; }
-        public int Damage { get; set; }
+        /// <summary>
+        /// Текущий показатель здоровья
+        /// </summary>
         public int CurHealth { get; set; }
+
+        /* Показатели урона */
+
+
+        public int MaxAD { get; set; }
+        public int CurAD { get; set; }
+        public int MaxAP { get; set; }
+        public int CurAP { get; set; }
+
+        /* Показатели защиты */
         public int MaxArmor { get; set; }
-        public int MaxMagicResist { get; set; }
         public int CurArmor { get; set; }
+        public int MaxMagicResist { get; set; }
         public int CurMagicResist { get; set; }
+
+        /* Показатели скорости передвижения */
+        public int MaxSpeed { get; set; }
+        public int CurSpeed { get; set; }
+        public double MaxAttackSpeed { get; set; }
+        public double CurAttackSpeed { get; set; } 
     }
 }
