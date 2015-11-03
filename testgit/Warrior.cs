be728fec8e98a03d9a034c96a03c485328b2eb1c
nickraz <obik.paren@gmail.com>
2015-11-03 14:14:41 +0300

    Добавление базовый характеристик героев

--- testgit/Warrior.cs
+++ testgit/Warrior.cs
@@ -7,5 +7,12 @@ namespace testgit
 {
     class Warrior
     {
+        public int MaxHealth { get; set; }
+        public int Damage { get; set; }
+        public int CurHealth { get; set; }
+        public int MaxArmor { get; set; }
+        public int MaxMagicResist { get; set; }
+        public int CurArmor { get; set; }
+        public int CurMagicResist { get; set; }
     }
 }
